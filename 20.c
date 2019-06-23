d28687cb44f3c045faee2704919526f547b3dc14
koushik08 <46640361+koushik08@users.noreply.github.com>
2019-06-23 22:24:34 +0530

    Update 20.c (#11)
    this gets a clear view

--- 20.c
+++ 20.c
@@ -30,16 +30,18 @@ int main(int argc, char **argv)
 	scanf("%d",&n);
 	int a[n];
 	for (i=0;i<n;i++){
-		scanf("%d",&a[i]);
+		scanf(" %d",&a[i]);
 	}
 	for (i=0;i<n-1;i++){
+		
 		for(j=i+1;j<n;j++){
+			
 			if (a[i]>a[j] && i<j){
 				f=f+1;
 			}
 		}
 	}
-	printf("%d",f);
+	printf(" %d ",f);
 	return 0;
 }
 
