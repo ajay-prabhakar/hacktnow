a99656e2057de6583f45ec4aec2b390a4acf8701
peddireddyhariharan <52133743+peddireddyhariharan@users.noreply.github.com>
2019-06-23 22:20:53 +0530

    Patch 1 (#8)
    * Update 17.c
    
    * Update 30p.c

--- 17.c
+++ 17.c
@@ -1,23 +1,23 @@
 #include <stdio.h> //union
 int main()
 {
-int a[10], b[10], flag = 0, n1, n2, i, j;
+int a[10], b[10], flag = 0, k1, k2, i, j;
 printf("Enter array1 size : ");
-scanf("%d",&n1);
+scanf("%d",&k1);
 printf("\nEnter array2 size : ");
-scanf("%d",&n2);
+scanf("%d",&k2);
 printf("\nEnter array1 element : ");
-for(i = 0;i < n1;i++)
+for(i = 0;i < k1;i++)
 scanf("%d",&a[i]);
 printf("\nEnter array2 element : ");
-for(i = 0;i < n2;i++)
+for(i = 0;i < k2;i++)
 scanf("%d",&b[i]);
 printf("\nUnion:");
-for(i = 0;i < n1;i++)
+for(i = 0;i < k1;i++)
 printf("%d, ",a[i]);
-for(i = 0;i < n2;i++)
+for(i = 0;i < k2;i++)
 {
-for(j = 0;j < n1;j++)
+for(j = 0;j < k1;j++)
 {
 if(b[i] == a[j])
 {
