6fcad5a86899559996fa13807ee23be870211719
Koushik <46640361+koushik08@users.noreply.github.com>
2019-10-01 23:47:46 +0530

    Update 25.c

--- 25.c
+++ 25.c
@@ -6,10 +6,11 @@
 int main() {
     int n,k;
     
-    scanf("%d  %d",&n,&k);
-    n=n>>k;
+    scanf("%d  %d",&n,&k)  ;
+    n=n>>k; 
     n=n%2;
-    printf("%d",n);
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    printf("  %d   ",n);
+    
+     /* Enter your code here. Read input from STDIN. Print output  dfmbdflmbdf  pmfv fs vto STDOUT */    
     return 0;
 }
