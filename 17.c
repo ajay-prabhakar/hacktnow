0dfc83ca9cd08c8d0398113be16774daa971889c
peddireddyhariharan <52133743+peddireddyhariharan@users.noreply.github.com>
2019-10-01 23:42:10 +0530

    Update 17.c

--- 17.c
+++ 17.c
@@ -1,32 +1,35 @@
 #include <stdio.h> //union
 int main()
 {
-int a[10], b[10], flag = 0, k1, k2, i, j;
+int c[10], d[10], flag = 0, k1, k2, i, j
+  
 printf("Enter array1 size : ");
+  
 scanf("%d",&k1);
 printf("\nEnter array2 size : ");
 scanf("%d",&k2);
 printf("\nEnter array1 element : ");
 for(i = 0;i < k1;i++)
-scanf("%d",&a[i]);
+scanf("%d",&c[i]);
 printf("\nEnter array2 element : ");
 for(i = 0;i < k2;i++)
-scanf("%d",&b[i]);
+scanf("%d",&d[i]);
 printf("\nUnion:");
+  
 for(i = 0;i < k1;i++)
-printf("%d, ",a[i]);
+printf("%d, ",c[i]);
 for(i = 0;i < k2;i++)
 {
 for(j = 0;j < k1;j++)
 {
-if(b[i] == a[j])
+if(d[i] == c[j])
 {
 flag = 1;
 }
 }
 if(flag == 0)
 {
-printf("%d, ",b[i]);
+printf("%d, ",d[i]);
 }
 flag = 0;
 }
