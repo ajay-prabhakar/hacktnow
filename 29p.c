70685bd44212d00818ba5df06b9c0c78f0d5cb47
Koushik <46640361+koushik08@users.noreply.github.com>
2019-10-01 23:46:04 +0530

    Update 29p.c (#24)

--- 29p.c
+++ 29p.c
@@ -4,13 +4,16 @@ int main()
    int* pc, n;
    
    n = 22;
+   
+   
+   
+   
    printf("Address of c: %u\n", &n);
    printf("Value of c: %d\n\n", n);
    
    pc = &n;
-   printf("Address of pointer pc: %u\n", pc);
-   printf("Content of pointer pc: %d\n\n", *pc);
-   
+   printf("Address of  pc: %u\n", pc);
+   printf("Content of  pc: %d\n\n", *pc);
    n = 11;
    printf("Address of pointer pc: %u\n", pc);
    printf("Content of pointer pc: %d\n\n", *pc);
