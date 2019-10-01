1923cc59250b9278537dfc1a0e6c11728ac88336
peddireddyhariharan <52133743+peddireddyhariharan@users.noreply.github.com>
2019-10-01 23:44:10 +0530

    Update 20.c (#23)

--- 20.c
+++ 20.c
@@ -28,15 +28,17 @@ int main(int argc, char **argv)
 {
 	int i,j,n,f=0;
 	scanf("%d",&n);
-	int a[n];
+	int b[n];
 	for (i=0;i<n;i++){
-		scanf(" %d",&a[i]);
+		scanf(" %d",&b[i]);
 	}
 	for (i=0;i<n-1;i++){
 		
-		for(j=i+1;j<n;j++){
+		
+		for(j=i+1;j<n;j
+		    
 			
-			if (a[i]>a[j] && i<j){
+			if (b[i]>b[j] && i<j){
 				f=f+1;
 			}
 		}
