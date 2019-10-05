210ab5097e6bdcc677c7667e8427423f4cd5e2d5
yashkumar683 <54894607+yashkumar683@users.noreply.github.com>
2019-10-05 19:01:45 +0530

    Update 25.c

--- 25.c
+++ 25.c
@@ -9,6 +9,7 @@ int main() {
     scanf("%d  %d",&n,&k)  ;
     n=n>>k; 
     n=n%2;
+   
     printf("  %d   ",n);
     
      /* Enter your code here. Read input from STDIN. Print output  dfmbdflmbdf  pmfv fs vto STDOUT */    
