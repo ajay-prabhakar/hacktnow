b98bd89507ad294741092471b7214ab86e1200a3
akhileshsachin <56290319+akhileshsachin@users.noreply.github.com>
2019-10-08 11:36:24 +0530

    Arithmetic op's

---
+++ arithemeticoperations.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+int a,b;
+cin>>a>>b;
+int add=a+b;
+int sub=a-b;
+int mul=a*b;
+int div=a/b;
+int rem=a%b;
+cout<<add<<endl;
+cout<<sub<<endl;
+cout<<mul<<endl;
+cout<<div<<endl;
+cout<<rem<<endl;
+
+}
